b1f03199f1607c4c1c16147a6a581443268a1c9d
Natasha Kholgade <natasha.kholgade@gmail.com>
2011-11-05 14:30:30 -0400

    measurement model

---
+++ measurementModelTranslation.m
@@ -0,0 +1,15 @@
+function z=measurementModelTranslation(p_world_IMU, p_IMU_camera, q_world_IMU, q_IMU_camera, p_world_pts, K)
+
+% z=measurementModelTranslation(p_world_IMU, p_IMU_camera, q_world_IMU, q_IMU_camera, p_world_pts, K)
+%
+% p_world_IMU: 3x1 vector
+% p_IMU_camera: 3x1 vector
+% q_world_IMU: 4x1 vector (unit quat)
+% q_IMU_camera: 4x1 vector (unit quat)
+% p_world_pts: 3xP points
+% K: 3x3 matrix
+%
+% z: 2xP points
+
+
+end
